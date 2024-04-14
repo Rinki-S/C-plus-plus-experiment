9eb232a94b6e3063abc9e19e043db7bbb2d48486
Fuze1111 <151722810+Fuze1111@users.noreply.github.com>
2024-04-14 22:58:43 +0800

    :white_check_mark: shit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Test" << std::endl;
     std::cout << "Test2" << std::endl;
+    std::cout << "Test3" << std::endl; 
     std::cout << ".gitignore test" << std::endl;
     return 0;
 }
