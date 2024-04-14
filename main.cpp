1b8d82f9677e00b186297db52bf4a313ae3e8e32
Fuze1111 <151722810+Fuze1111@users.noreply.github.com>
2024-04-14 22:52:35 +0800

    :white_check_mark: Add test.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Test" << std::endl;
+    std::cout << "Test2" << std::endl;
     std::cout << ".gitignore test" << std::endl;
     return 0;
 }
