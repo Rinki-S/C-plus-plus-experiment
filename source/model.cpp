614553f01ca2a70cf30593c2e5345a9e19757af5
Fuze1111 <151722810+Fuze1111@users.noreply.github.com>
2024-04-16 21:48:48 +0800

    :+1: Add two function:save to file,load from file

--- source/model.cpp
+++ source/model.cpp
@@ -1,8 +1,26 @@
 #include <fstream>
 
-std::ofstream file("studentsList.txt");
-for (auto& student : students) {
-	file << student.getID() << " " << student.getName() << " " << student.getUsualScore() << " " << student.getExamScore() << " " << student.getFinalScore() << std::endl;
+void saveToFile(const std::vector<Student>& students) {
+	std::ofstream file("studentsList.txt");
+	for (auto& student : students) {
+		file << student.getID() << " " << student.getName() << " " << student.getUsualScore() << " " << student.getExamScore() << " " << student.getFinalScore() << std::endl;
+	}
+	file.close();
 }
 
-file.close();
+void loadFromFile(std::vector<Student>& students) {
+	std::ifstream file("studentsList.txt");
+	if (!file) {
+		std::cout << "No file found." << std::endl;
+		return;
+	}
+	std::string id;
+	std::string name;
+	double usualScore;
+	double examScore;
+	double finalScore;
+	while (file >> id >> name >> usualScore >> examScore >> finalScore) {
+		students.emplace_back(id, name, usualScore, examScore);
+	}
+	file.close();
+}
